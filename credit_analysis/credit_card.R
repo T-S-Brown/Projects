94f1a677fe655af44fe4f82083a5b0b3637f8acc
Thomas Brown <tom.southwell.brown@gmail.com>
2018-02-22 21:31:35 +0000

    analysis development

--- credit_analysis/credit_card.R
+++ credit_analysis/credit_card.R
@@ -6,14 +6,18 @@
 library(tidyverse)
 library(caret)
 
-set.seed(8589)
+set.seed(8549)
 
 # Load into memory the source dataset from kaggles
-source <- read_csv("/Users/Thomas/Downloads/creditcard.csv")
+source <- read.csv("/Users/Thomas/Downloads/creditcard.csv")
 
 
 # Preview the data
 head(source)
+
+# Check the target split
+table(source$Class)
+sum(table(source$Class[2])) / length(source$Class)
 
 # Perform a train/test split
 
@@ -26,7 +30,7 @@ test <- source[-data_index,]
 rm(source)
 
 
-# Data Preprocessing
+# Data Preprocessing: Not Needed due to PCA
 
 
 
@@ -34,13 +38,50 @@ rm(source)
 # Stage 1: Exploratory Data Analysis
 #---------------------------------------#
 
-# Descriptive Statistics
+# Descriptives
 
-descriptives <- summarise_all(train, sd)
+descriptives <- train %>%
+    group_by(Class) %>% 
+    summarise_all(mean)
 
-# Check class balance
 
 # Quick and Dirty Evaluation of the principal components - correlation plot
+
+correlations <- train %>% 
+    summarize_all(funs(cor(., train$Class))) %>% 
+    t()
+
+colnames(correlations) <- "coef"
+
+correlations <- data.frame(correlations)
+correlations$feature <- rownames(correlations)
+rownames(correlations) <- 1:nrow(correlations)
+
+correlations <- mutate(correlations,
+                       direction = case_when(
+                           coef >= 0 ~ "Positive",
+                           TRUE ~ "Negative"
+                       ),
+                       magnitude = abs(coef)) %>% 
+    filter(feature != "Class")
+
+
+correlations$feature <- factor(correlations$feature) %>%
+    fct_reorder(correlations$magnitude)
+
+exploratory_plot <- ggplot(data = correlations, aes(x = feature, y = magnitude, fill = direction)) +
+    geom_bar(stat = "identity") +
+    coord_flip() +
+    labs(x = "Magnitude of Coefficient",
+         y = "Feature",
+         title = "Correlations with outcome",
+         fill = "Coefficient Direction") +
+    theme(plot.title = element_text(hjust = 0.5))
+
+exploratory_plot
+
+exploratory_plot + ylim(0, 1)
+
 
 
 #---------------------------------------#
@@ -48,15 +89,25 @@ descriptives <- summarise_all(train, sd)
 #          construction
 #---------------------------------------#
 
+train$Class <- factor(train$Class)
+levels(train$Class) <- c("Fraud", "Genuine")
+
+
+# Model 1: Logistic Regression --------------------------------------------
+
+
+
 
 # Set up a baseline model: Logistic Regression
 
 # Set up 5 fold cross validation
 
-tc <- trainControl("cv", 5, savePredictions = TRUE)
+tc <- trainControl("cv", 5,
+                   savePredictions = TRUE,
+                   classProbs = TRUE)
 
 
-fit <- train(as.factor(Class) ~.,
+fit <- train(factor(Class) ~.,
              data      = train    ,
              method    = "glm"    ,
              family    = binomial ,
@@ -64,4 +115,8 @@ fit <- train(as.factor(Class) ~.,
              na.action = na.omit)
 
 results <- round(summary(fit)$coefficients, 3)
+
+
+# Model 2: Random Forest --------------------------------------------------
+
 
