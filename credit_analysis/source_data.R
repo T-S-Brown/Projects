22601f841eef8d72b70a18871655004b7b895168
Thomas Brown <tom@tsbrown.net>
2018-10-03 20:52:41 +0100

    add source data file

---
+++ credit_analysis/source_data.R
@@ -0,0 +1,13 @@
+#-------------------------------------------#
+# ML Equity Project
+# Sourcing data
+#
+# Connects to APIs to download key data
+#
+#-------------------------------------------#
+
+library(tidyverse)
+
+
+
+# Load the API keys
