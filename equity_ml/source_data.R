6acff5dc7fdfcd828c9c2df3e969f347fe6c45ed
Thomas Brown <tom.southwell.brown@gmail.com>
2018-10-14 13:40:50 +0100

    update source data script

--- equity_ml/source_data.R
+++ equity_ml/source_data.R
@@ -6,8 +6,13 @@
 # Performs first order data wrangling
 #-------------------------------------------#
 
+
+# Load the required packages
 library(tidyverse)
 
+# Set the working directory
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 
 
 # Load the API keys
+source('load_api_keys.R')
