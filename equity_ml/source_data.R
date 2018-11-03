4c2f6c555040c471bbac878bfb24b10daa52a37c
Thomas Brown <tom.southwell.brown@gmail.com>
2018-11-03 15:47:14 +0000

    create loop structure to load key data

--- equity_ml/source_data.R
+++ equity_ml/source_data.R
@@ -17,3 +17,37 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 # Load the API keys
 source('load_api_keys.R')
 
+
+# Specify the vector of codes to analysis
+equity_codes <- c("GSK.L", "LLOY.L", "LGEN.L")
+
+
+#-------------------------------------------#
+# Retrieve the key price/volume data
+#-------------------------------------------#
+
+i <- 1
+for(code in equity_codes) {
+  
+  data <- read_csv(url(paste0('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY',
+                              '&symbol=',code,
+                              '&outputsize=full',
+                              '&apikey=',av_key,'&datatype=csv')))
+  
+  data <- mutate(data, code = symbol)
+  
+  if(i == 1) {
+    core_data <- data
+  } else {
+    core_data <- bind_rows(core_data, data)
+  }
+  
+  
+  print(paste0(code,' loaded    ', i, '/', length(equity_codes), ' complete'))
+  
+  i <- i + 1
+}
+
+print('Key price and volume data loaded')
+
+
