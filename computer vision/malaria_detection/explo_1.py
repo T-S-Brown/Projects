3d8de307611948e7ca91ac67ce034cc96c534602
Thomas Brown <thomas.brown@aire.io>
2019-05-03 16:16:31 +0100

    update

---
+++ computer vision/malaria_detection/explo_1.py
@@ -0,0 +1,12 @@
+# Malaria Cell Detection
+
+# Module Imports
+from fastai.vision import *
+
+# Check Installation 
+from fastai.utils import *
+show_install()
+
+import numpy as np
+
+x = np.array([1, 2, 3, 4, 5])
